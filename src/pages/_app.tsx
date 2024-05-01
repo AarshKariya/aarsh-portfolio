50ce592f32972707f2708f7ae672cbc3ffc3cd3d
aarshmittilabs <aarsh@mittilabs.earth>
2024-05-01 09:31:59 +0530

    feat: transition page animation

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,21 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import NavigationBar from "./components/NavigationBar/NavigationBar";
 
+import { useRouter } from "next/router";
+import { AnimatePresence, motion } from "framer-motion";
+import Transition from "./components/Transition/Transition";
+
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
   return (
     <div className="bg-[#FEFBF6]">
-      <Component {...pageProps} />
-      <NavigationBar />
+      <AnimatePresence mode="wait">
+        <motion.div key={router.route} className="h-full">
+          <Transition />
+          <Component {...pageProps} />
+          <NavigationBar />
+        </motion.div>
+      </AnimatePresence>
     </div>
   );
 }
