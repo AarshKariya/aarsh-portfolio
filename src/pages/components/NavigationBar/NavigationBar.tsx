53ae777f6c074c3aa737eaa0bc2db55e6e4abad3
aarshmittilabs <aarsh@mittilabs.earth>
2024-05-01 09:17:06 +0530

    style: change navigation position to right side

--- src/pages/components/NavigationBar/NavigationBar.tsx
+++ src/pages/components/NavigationBar/NavigationBar.tsx
@@ -60,7 +60,7 @@ const NavigationBar = () => {
   const router = useRouter();
   const pathname = router.pathname;
   return (
-    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl-right-[2%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen ">
+    <nav className="flex flex-col items-center xl:justify-center gap-y-4 fixed h-max bottom-0 mt-auto xl:right-[1%] z-50 top-0 w-full xl:w-16 xl:max-w-md xl:h-screen ">
       <div className="flex w-full xl:flex-col items-center justify-between xl:justify-center gap-y-10 px-4 md:px-40 xl:px-0 h-[80px] xl:h-max py-8 bg-[#8CB9BD] bg-opacity-10 backdrop-blur-sm text-3xl xl:text-xl xl:rounded-full">
         {navData?.map((link, index) => {
           return (
@@ -68,8 +68,8 @@ const NavigationBar = () => {
               href={link.path}
               key={link.id}
               className={`${
-                link.path === pathname && "text-accent"
-              } relative flex items-center group hover:text-accent transition-all duration-300`}
+                link.path === pathname && "text-[#F6995C]"
+              } relative flex items-center group hover:text-[#F6995C] transition-all duration-300`}
             >
               <div>{link.icon}</div>
             </Link>
